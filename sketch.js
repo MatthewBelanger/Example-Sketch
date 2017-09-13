edf40d9de8156a6f65aad8ce886d3630e0bcd2ce
MatthewBelanger <31661435+MatthewBelanger@users.noreply.github.com>
2017-09-13 11:48:06 -0400

    Main files

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(50, 100, 100);
+    fill(10, 100, 250);
+    ellipse(mouseX, mouseY, 40, 40);
+    console.log("Check,Check");
+}
